58e9f9c840f6ddfa475b2876bab6026490b5b844
KetosXIII <59283368+KetosXIII@users.noreply.github.com>
2020-12-28 11:13:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myproject_4a_neret_thomas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myproject_4a_neret_thomas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
