864484d745beabb51d15a982d52d43b697072044
KetosXIII <59283368+KetosXIII@users.noreply.github.com>
2020-12-29 16:03:22 +0100

    finish mvvm

--- app/src/main/java/com/example/myproject_4a_neret_thomas/MainViewModel.kt
+++ app/src/main/java/com/example/myproject_4a_neret_thomas/MainViewModel.kt
@@ -4,8 +4,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel(){
-    val text:MutableLiveData<String> = MutableLiveData()
+    val counter : MutableLiveData<Int> = MutableLiveData()
     init {
-        text.value="Blabla"
+        counter.value = 0
+    }
+
+    fun onClickedIncrement () {
+        counter.value = (counter.value ?:0)+ 1
     }
 }
